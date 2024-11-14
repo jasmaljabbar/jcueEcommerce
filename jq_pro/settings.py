940098ebad5f96bd1e36a55f6764d42abb45ef7a
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-11-14 20:09:35 +0530

    add the sh and yaml file

--- jq_pro/settings.py
+++ jq_pro/settings.py
@@ -165,8 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
 
 # STORAGES = {
 #     # ...
@@ -174,6 +173,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
 #     },
 # }
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
